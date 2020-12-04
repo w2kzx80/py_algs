88fc43afae167ca08cf6b912b686ce821d87b1ea
Apanovici Roman <w2kzx80@gmail.com>
2020-12-05 00:45:46 +0200

    hw3

--- 3/6.py
+++ 3/6.py
@@ -4,17 +4,22 @@
 
 import random
 
-a1 = [random.randint(0,100) for i in range(5)]
+a1 = [random.randint(0,100) for i in range(10)]
 print(a1)
-min = a1[0]
-max = a1[0]
-sum = 0
-for v in a1:
-    if v > max:
-        max = v
-    if v < min:
-        min = v
-    sum += v
-sum = sum - max - min
+imin = 0
+imax = 0
+for k, v in enumerate(a1):
+    if v > a1[imax]:
+        imax = k
+    if v < a1[imin]:
+        imin = k
 
-print(sum)
+print (f"Min = {a1[imin]}\nMax = {a1[imax]}")
+if imin > imax:
+    imin, imax = imax, imin
+sum = 0
+for k in range(imin, imax):
+    if k!= imin:
+        sum += a1[k]
+
+print(f"Sum = {sum}")
